ebfbb11545c5c9e188ff97aeaa2a3e4e7f35064f
praveenkumar-11 <praveenkumar10082003@gmail.com>
2023-11-30 16:31:57 +0530

    Implemented error states

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -12,7 +12,17 @@ btn.addEventListener("click", () => {
     const errors= document.querySelectorAll("p.error");
     const labels= document.querySelectorAll("label");
 
+    
+    const user_date= new Date(year, month -1 , day);
+    console.log(user_date.getDate());
+
     for(let i=0 ; i<inputs.length ; i++){ 
+        
+        //Removing error states
+        inputs[i].classList.remove("error");
+        labels[i].classList.remove("error");
+        errors[i].innerHTML= "";
+
         if(inputs[i].value == ""){
             //labels[i].style.color= "#ff5757";
             inputs[i].classList.add("error");
@@ -22,23 +32,38 @@ btn.addEventListener("click", () => {
             console.log(this_date+","+ this_month+","+this_year);
 
         }
-
-        if(this_year < year){
-            errors[2].innerHTML= "Must be in the past";
-            inputs[i].classList.add("error");
-            labels[i].classList.add("error");
-        }
-
-        if(this_year == year && this_month < month){
-            errors[1].innerHTML= "Must be in the past";
-            inputs[i].classList.add("error");
-            labels[i].classList.add("error");
-        }
-
-        else{
+        /* else{
             inputs[i].classList.remove("error");
             labels[i].classList.remove("error");
             errors[i].innerHTML= "";
-        }
+        } */
+    }
+
+    if(this_year < year){
+        errors[2].innerHTML= "Must be in the past";
+        inputs[2].classList.add("error");
+        labels[2].classList.add("error");
+    }
+
+    if(this_year == year && this_month < month){
+        errors[1].innerHTML= "Must be in the past";
+        inputs[1].classList.add("error");
+        labels[1].classList.add("error");
+    }
+
+    if(day != "" && day != user_date.getDate()){
+        errors[0].innerHTML= "Must be a valid date";
+        inputs[0].classList.add("error");
+        labels[0].classList.add("error");
+    }
+
+    if((month != "") && (month > 12 || month < 1)){
+        errors[1].innerHTML= "Must be a valid month";
+        inputs[1].classList.add("error");
+        labels[1].classList.add("error");
     }
 })
+
+function calc_diff(){
+    
+}
