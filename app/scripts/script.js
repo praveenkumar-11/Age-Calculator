5c984ffb5db855883e2671934cef68be268a6549
praveenkumar-11 <praveenkumar10082003@gmail.com>
2023-11-30 17:06:07 +0530

    Implemented code to calculate the difference

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -12,6 +12,7 @@ btn.addEventListener("click", () => {
     const errors= document.querySelectorAll("p.error");
     const labels= document.querySelectorAll("label");
 
+    let isValid= true;
     
     const user_date= new Date(year, month -1 , day);
     console.log(user_date.getDate());
@@ -28,6 +29,7 @@ btn.addEventListener("click", () => {
             inputs[i].classList.add("error");
             labels[i].classList.add("error");
             errors[i].innerHTML= "This field is required";
+            isValid= false;
             /*inputs[i].style.border= '1px solid hsl(0, 100%, 67%)'; */
             console.log(this_date+","+ this_month+","+this_year);
 
@@ -43,27 +45,48 @@ btn.addEventListener("click", () => {
         errors[2].innerHTML= "Must be in the past";
         inputs[2].classList.add("error");
         labels[2].classList.add("error");
+        isValid= false;
     }
 
     if(this_year == year && this_month < month){
         errors[1].innerHTML= "Must be in the past";
         inputs[1].classList.add("error");
         labels[1].classList.add("error");
+        isValid= false;
+    }
+
+    if(this_year == year && this_month == month && this_date < day){
+        errors[0].innerHTML= "Must be in the past";
+        inputs[0].classList.add("error");
+        labels[0].classList.add("error");
+        isValid= false;
     }
 
     if(day != "" && day != user_date.getDate()){
         errors[0].innerHTML= "Must be a valid date";
         inputs[0].classList.add("error");
         labels[0].classList.add("error");
+        isValid= false;
     }
 
     if((month != "") && (month > 12 || month < 1)){
         errors[1].innerHTML= "Must be a valid month";
         inputs[1].classList.add("error");
         labels[1].classList.add("error");
+        isValid= false;
+    }
+
+    if(isValid){
+        calc_diff(user_date)
     }
 })
 
-function calc_diff(){
-    
+function calc_diff(user_date){
+
+    const result= querySelectorAll(".result");
+    const diff_years= today.getFullYear() - user_date.getFullYear();
+    const diff_months= today.getMonth() - user_date.getMonth();
+    const diff_days= today.getDate() - user_date.getDate();
+    console.log("Diff: "+diff_days);
+
 }
